cf72b877a5c64a3e4c328e6eedff72d281bf5319
Melson Zacharias <jmelson7@gmail.com>
2018-01-20 16:18:57 +0530

    big bang

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.0.1 January 2018
+Initial Commit
+
---
+++ src/__tests__/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "react-native";
+
+import App from "../../App";
+
+// Note: test renderer must be required after react-native.
+import * as renderer from "react-test-renderer";
+it("renders correctly", () => {
+  const tree = renderer.create(<App />);
+  expect(tree).toBeDefined();
+});
